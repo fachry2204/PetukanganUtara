5a6d6c48c499ca3d6216648f7aceaa06a5170639
mvpcyber <mvpcyberjakarta@gmail.com>
2026-05-11 16:11:03 +0700

    update system

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const db = require('./db');
 const waService = require('./services/whatsappService');
 
 // Initialize WA Service
-waService.init();
+// waService.init(); // Matikan sementara untuk tes di hosting agar tidak berat/error di hosting
 
 const app = express();
 const PORT = process.env.PORT || 5000;
